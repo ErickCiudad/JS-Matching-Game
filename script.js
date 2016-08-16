90ba299c5cf1caeec350da4a95bed8497a79ecd2
Start Coding Student 2 <session2@START-Coding-MBP-08.local>
2016-08-15 18:01:45 -0700

    Spawns 5 on one side and 6 on the other, randomly

--- script.js
+++ script.js
@@ -2,10 +2,53 @@ var numberOfPics = 5;
 var theLeftSide = document.getElementById('leftSide');
 
 
-function img(){
+var p = Math.random() * 400;
+var m = Math.round(p);
+//margin top position
 
+var a = Math.random() * 100;
+var b = Math.round(a);
+//margin left position
+
+function img(){
 var elem = document.createElement("img");
-elem.src = 'https://67.media.tumblr.com/18353a4e1bd6c01e2b21253ab3c64dfc/tumblr_njts10Gzgf1snc5kxo1_500.gif';
+elem.src = 'doughnut.gif';
+elem.style.left = m + "px";
+elem.style.top = b + "%";
+elem.id = "findTheImage"
+elem.addEventListener('click', imgTwo())
 document.getElementById("leftSide").appendChild(elem);
     }
 img();
+
+function imgTwo(){
+  for(var i = 0; i < 5; i++){
+    var p = Math.random() * 400;
+    var m = Math.round(p);
+    //margin top position
+
+    var a = Math.random() * 100;
+    var b = Math.round(a);
+    //margin left position
+    var elem = document.createElement("img");
+    elem.src = 'doughnut.gif';
+    elem.style.left = m + "px";
+    elem.style.top = b + "%";
+    elem.id = "crowdImage"
+    document.getElementById('leftSide').appendChild(elem);
+
+    // elem.addEventListener('click', imgTwo())
+    //SECOND ELEMENT
+    var elem2 = document.createElement("img");
+    elem2.src = 'doughnut.gif';
+    elem2.style.left = m + "px";
+    elem2.style.top = b + "%";
+    elem2.id = "crowdImage"
+    document.getElementById('rightSide').appendChild(elem2);
+
+    // document.getElementById("rightSide").appendChild(elem);
+  }
+  ///Right Side Now
+
+
+}
